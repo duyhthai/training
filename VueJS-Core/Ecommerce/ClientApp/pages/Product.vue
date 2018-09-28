47bb98863262be9d852c3bcd8b0d1ec60d4a5cb1
Duy <duyhthai@gmail.com>
2018-09-28 11:48:40 +0700

    fetching data before navigation

--- VueJS-Core/Ecommerce/ClientApp/pages/Product.vue
+++ VueJS-Core/Ecommerce/ClientApp/pages/Product.vue
@@ -1,5 +1,7 @@
 <template>
-  <product-details :product="product" />
+<div class="page">
+    <product-details :product="product" />
+</div>
 </template>
 
 <script>
@@ -12,19 +14,22 @@ export default {
   },
   data() {
     return {
-      product: null
+      product: {}
     };
   },
-  mounted() {
-    const slug = this.$route.params.slug;
-
-    fetch(`/api/products/${slug}`)
+  methods: {
+    setData(product) {
+        this.product = product;
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    fetch(`/api/products/${to.params.slug}`)
         .then(response => {
             return response.json();
         })
         .then(product => {
-            this.product = product;
+            next(vm => vm.setData(product));
         });
-    }
+  }
 };
 </script>
