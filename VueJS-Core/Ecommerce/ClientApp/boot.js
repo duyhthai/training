97d7bcf7c08ef699cfadf015686b1a4469dd577a
Duy <duyhthai@gmail.com>
2018-09-28 10:10:39 +0700

    add sass, bootstrap-vue, nprogress

--- VueJS-Core/Ecommerce/ClientApp/boot.js
+++ VueJS-Core/Ecommerce/ClientApp/boot.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import BootstrapVue from "bootstrap-vue";
 
 Vue.use(VueRouter);
+Vue.use(BootstrapVue);
 
 // import page components
 import Catalogue from "./pages/Catalogue.vue";
